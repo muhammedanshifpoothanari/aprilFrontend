5ba04d3249a3c18aff1c9590ed7262f09ffc2901
muhammedanshifpoothanari <121426946+muhammedanshifpoothanari@users.noreply.github.com>
2024-04-05 16:45:56 +0530

    Add files via upload

---
+++ src/components/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const dispatch = useDispatch()
+  const navigator = useNavigate()
+  const logout = () => {
+    dispatch({ type: 'SET_USER', payload: 'user not found' }); 
+    navigator('/login')
+  }
+  const user = useSelector((state) => state.user.user);
+
+  return (
+    <div>
+      <h2>Welcome, {user}!</h2>
+      <p>This is your home page.</p>
+      <button onClick={logout}>logout</button>
+    </div>
+  );
+};
+
+export default Home;
